99d53d23c3fafaa8e23bdcece311da8c5d299f84
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2024-01-22 10:41:09 -0300

    CRUD adicionado

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,39 @@
+import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 
 function Navbar() {
- 
-  
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('Usuário deslogado com sucesso')
+    navigate('/login')
+  }
+
+  let navbarComponent
+
 
   return (
     <>
-     <div className='w-full bg-[#3f93ff] text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Dev Web</div>
+      <div className='w-full bg-[#3f93ff] text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Dev Web</Link>
 
-            <div className='flex gap-4'>
-              <Link to='/login' className='font-bold cursor-pointer hover:text-[#242b2d]'>Login</Link>
-              <Link to='/home' className='font-bold cursor-pointer hover:text-[#242b2d]'>Home</Link>
-              <div className='font-bold cursor-pointer text-[#fde845] hover:text-[#2d2c24] animate-pulse'>Postagens</div>
-              <div className='font-bold cursor-pointer hover:text-[#242b2d]'>Temas</div>
-              <div className='font-bold cursor-pointer hover:text-[#242b2d]'>Cadastrar tema</div>
-              <div className='font-bold cursor-pointer hover:text-[#242b2d]'>Perfil</div>
-              <div className='font-bold cursor-pointer hover:text-[#242b2d]'>Sair</div>
-            </div>
+          <div className='flex gap-4'>
+            <div className='font-bold cursor-pointer text-[#fde845] hover:text-[#2d2c24] animate-pulse'>Postagens</div>
+            <Link to='/temas' className='font-bold cursor-pointer hover:text-[#242b2d]'>Temas</Link>
+            <Link to='/cadastroTema' className='font-bold cursor-pointer hover:text-[#242b2d]'>Cadastrar tema</Link>
+            <div className='font-bold cursor-pointer hover:text-[#242b2d]'>Perfil</div>
+            <Link to='' onClick={logout} className='font-bold cursor-pointer hover:text-[#242b2d]'>Sair</Link>
+
           </div>
         </div>
+      </div>
     </>
   )
 }
