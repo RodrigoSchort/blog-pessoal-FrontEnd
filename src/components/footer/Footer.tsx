df4ec05def6b9c52a99d6b13cb9c26d4bbdcd1d4
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2024-01-31 09:53:50 -0300

    add target blank

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -18,13 +18,13 @@ function Footer() {
             <p className='text-xl'>Blog pessoal Dev Web | Copyright: Dev RodrigoSchort {data}</p>
             <p className='text-lg'>Acesse minhas redes sociais</p>
             <div className='flex gap-2'>
-              <a href="https://www.linkedin.com/in/rodrigo-schort/">
+              <a href="https://www.linkedin.com/in/rodrigo-schort/" target="_blank">
                 <LinkedinLogo size={48} />
               </a>
-              <a href="https://www.instagram.com/rodrigo_schort?igsh=eGgydTFranc5bm5s">
+              <a href="https://www.instagram.com/rodrigo_schort?igsh=eGgydTFranc5bm5s" target="_blank">
                 <InstagramLogo size={48} />
               </a>
-              <a href="https://www.facebook.com/rodrigo.schort.94/">
+              <a href="https://www.facebook.com/rodrigo.schort.94/" target="_blank">
                 <FacebookLogo size={48} />
               </a>
             </div>
